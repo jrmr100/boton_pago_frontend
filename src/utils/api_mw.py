3e8893c5d7fe8fddc736b23b7f373212565fbeff
jmonroy <jrmr100@gmail.com>
2025-01-28 22:38:10 -0400

    refact: funcion connect de api_mw

--- src/utils/api_mw.py
+++ src/utils/api_mw.py
@@ -1,3 +1,5 @@
+from sqlite3 import connect
+
 import requests
 import json
 from dotenv import load_dotenv
@@ -6,6 +8,22 @@ import logging
 
 logger = logging.getLogger(__name__)
 load_dotenv()
+
+
+def connect_mw(headers, body, endpoint):
+    headers = headers
+    body = body
+    endpoint = endpoint
+    try:
+        response = requests.post(endpoint,
+                                 headers=headers, json=body,
+                                 timeout=15)
+        response_decode = response.content.decode("utf-8")
+        datos_response = json.loads(response_decode)
+        return datos_response
+    except Exception as e:
+        return e
+
 
 class ApiMw:
     def __init__(self, client_id):
@@ -16,12 +34,5 @@ class ApiMw:
         body = {"token": os.getenv("TOKEN_MW"), "cedula": self.client_id}
         endpoint = os.getenv("ENDPOINT_BASE") + os.getenv("ENDPOINT_BUSCAR_CLIENTE")
 
-        try:
-            response = requests.post(endpoint,
-                                     headers=headers, json=body,
-                                     timeout=15)
-            response_decode = response.content.decode("utf-8")
-            datos_response = json.loads(response_decode)
-            return datos_response
-        except Exception as e:
-            return e
+        cliente = connect_mw(headers, body, endpoint)
+        return cliente
