68add6cab434ce708092797c453ee6da8ab6b6af
“jrmr100” <jrmr100@gmail.com>
2025-02-24 14:29:16 -0400

    feat: spinner inicial

---
+++ src/static/js/spinner_modal.js
@@ -0,0 +1,4 @@
+    document.querySelector('form').addEventListener('submit', function (event) {
+    var loadingModal = new bootstrap.Modal(document.getElementById('loadingModal'));
+    loadingModal.show();
+});
