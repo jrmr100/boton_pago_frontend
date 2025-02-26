f31b2f522d3bebcc8fcf29327d685716015185d2
“jrmr100” <jrmr100@gmail.com>
2025-02-26 16:00:55 -0400

    feat: cambios en logger

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -13,8 +13,7 @@ now = datetime.now()
 today = now.strftime('%d%m%Y')
 
 logging.basicConfig(handlers=[logging.FileHandler(filename=os.getenv("PATH_BASE") + os.getenv("LOG_FILE") +
-                                                           os.getenv("NOMBRE_PROYECTO") +
-                                                  "_" + today + ".log",
+                                                           os.getenv("NOMBRE_PROYECTO") + "_" + today + ".log",
                     encoding='utf-8', mode='a+')],
                     level=int(os.getenv("LOG_LEVEL")),
                     format='%(asctime)s %(levelname)s %(name)s %(message)s')
