8994c521f76de20f576118168767cf74b11298b0
jmonroy <jrmr100@gmail.com>
2025-05-18 16:10:24 -0400

    feat: disable banco pm

--- src/routes/pagomovil_bp/templates/form_fields_bancos.py
+++ src/routes/pagomovil_bp/templates/form_fields_bancos.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField
+from src.config import pm_banesco, pm_bancoplaza
 
 
 class FormFieldsBancos(FlaskForm):
-    submit_bancoplaza = SubmitField('REPORTAR PAGO')
-    submit_banesco = SubmitField('REPORTAR PAGO')
+    submit_bancoplaza = SubmitField('REPORTAR PAGO', render_kw={'disabled': pm_bancoplaza[4]})
+    submit_banesco = SubmitField('REPORTAR PAGO', render_kw={'disabled': pm_banesco[4]})
 
