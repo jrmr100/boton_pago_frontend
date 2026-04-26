490cf015c913700396547effc44cb86df908d618
jmonroy <jrmr100@gmail.com>
2026-04-26 14:40:35 -0400

    feat: wsgi

---
+++ src/wsgi.py
@@ -0,0 +1,4 @@
+import sys
+sys.path.insert(0, '/var/www/boton_pago_frontend/')
+sys.path.append('/var/www/boton_pago_frontend/.venv/lib/python3.9/site-packages/')
+from src.app import app as application
